44dcc1f69441d48a6dc3c7a6a5c7c85a82bfce15
Bunma93 <bunma93@gmail.com>
2024-07-25 10:05:01 +0700

    Js-arr43

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // write code here
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const filteredArray = filterRange(arr, 3, 7);
+
+function filterRange(arr, a ,b) {
+    let result = arr.filter(x => x >= a && x <= b)
+    return result
+};
+
+console.log(filteredArray);
